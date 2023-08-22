9871647c36f21993747c54f32a24ae590893d0d0
kztTateyamaSbi <kzt.tateyama@gmail.com>
2023-08-22 15:31:37 +0900

    Add test codes for states.

--- contracts/src/test/java/com/r3/developers/apples/states/AppleStampTest.java
+++ contracts/src/test/java/com/r3/developers/apples/states/AppleStampTest.java
@@ -1,9 +1,13 @@
 package com.r3.developers.apples.states;
 
+import com.r3.developers.apples.TestUtils;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
+import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
+import java.util.ArrayList;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -58,7 +62,48 @@ class AppleStampTest {
         assertTrue(holderField.getType().isAssignableFrom(PublicKey.class));
     }
 
+    /**
+     * Task 5.
+     * TODO: Check to see if the AppleState's issue field is included in the participants.
+     */
     @Test
-    void getParticipants() {
+    void issuerIsParticipant() throws NoSuchAlgorithmException {
+
+        ArrayList<PublicKey> publicKeys = new ArrayList<>();
+
+        KeyPair issuerKeys = TestUtils.generateKey();
+        publicKeys.add(issuerKeys.getPublic());
+
+        KeyPair holderKeys = TestUtils.generateKey();
+        publicKeys.add(holderKeys.getPublic());
+
+        AppleStamp appleStamp = new AppleStamp(UUID.randomUUID(), "This is a test stamp.",
+                issuerKeys.getPublic(), holderKeys.getPublic());
+
+        /* リストに期待したオブジェクトが含まれない場合、indexOfメソッドは-1を返す. */
+        assertNotEquals(appleStamp.getParticipants().indexOf(issuerKeys.getPublic()), -1);
     }
+
+    /**
+     * Task 6.
+     * TODO: Check to see if the AppleState's holder field is included in the participants.
+     */
+    @Test
+    void holderIsParticipant() throws NoSuchAlgorithmException {
+
+        ArrayList<PublicKey> publicKeys = new ArrayList<>();
+
+        KeyPair issuerKeys = TestUtils.generateKey();
+        publicKeys.add(issuerKeys.getPublic());
+
+        KeyPair holderKeys = TestUtils.generateKey();
+        publicKeys.add(holderKeys.getPublic());
+
+        AppleStamp appleStamp = new AppleStamp(UUID.randomUUID(), "This is a test stamp.",
+                issuerKeys.getPublic(), holderKeys.getPublic());
+
+        /* リストに期待したオブジェクトが含まれない場合、indexOfメソッドは-1を返す. */
+        assertNotEquals(appleStamp.getParticipants().indexOf(holderKeys.getPublic()), -1);
+    }
+
 }
--- contracts/src/test/java/com/r3/developers/apples/states/BasketOfApplesTest.java
+++ contracts/src/test/java/com/r3/developers/apples/states/BasketOfApplesTest.java
@@ -1,9 +1,13 @@
 package com.r3.developers.apples.states;
 
+import com.r3.developers.apples.TestUtils;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.lang.reflect.Field;
+import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
+import java.util.ArrayList;
 
 class BasketOfApplesTest {
 
@@ -55,4 +59,81 @@ class BasketOfApplesTest {
         assertTrue(weightField.getType().isAssignableFrom(Integer.class));
     }
 
+    /**
+     * Task 5.
+     * TODO: Check to see if the BasketOfApples's farm field is included in the participants.
+     */
+    @Test
+    void farmIsParticipant() throws NoSuchAlgorithmException {
+
+        ArrayList<PublicKey> publicKeys = new ArrayList<>();
+
+        KeyPair farmKeys = TestUtils.generateKey();
+        publicKeys.add(farmKeys.getPublic());
+
+        KeyPair ownerKeys = TestUtils.generateKey();
+        publicKeys.add(ownerKeys.getPublic());
+
+        BasketOfApples basketOfApples = new BasketOfApples( "Fuji, Aomori.",
+                farmKeys.getPublic(), ownerKeys.getPublic(), 100);
+
+        /* リストに期待したオブジェクトが含まれない場合、indexOfメソッドは-1を返す. */
+        assertNotEquals(basketOfApples.getParticipants().indexOf(farmKeys.getPublic()), -1);
+    }
+
+    /**
+     * Task 6.
+     * TODO: Check to see if the AppleState's owner field is included in the participants.
+     */
+    @Test
+    void ownerIsParticipant() throws NoSuchAlgorithmException {
+
+        ArrayList<PublicKey> publicKeys = new ArrayList<>();
+
+        KeyPair farmKeys = TestUtils.generateKey();
+        publicKeys.add(farmKeys.getPublic());
+
+        KeyPair ownerKeys = TestUtils.generateKey();
+        publicKeys.add(ownerKeys.getPublic());
+
+        BasketOfApples basketOfApples = new BasketOfApples( "Fuji, Aomori.",
+                farmKeys.getPublic(), ownerKeys.getPublic(), 100);
+
+        /* リストに期待したオブジェクトが含まれない場合、indexOfメソッドは-1を返す. */
+        assertNotEquals(basketOfApples.getParticipants().indexOf(ownerKeys.getPublic()), -1);
+    }
+
+    /**
+     * Task 7.
+     * TODO: Verified that the helper method to update Owner functions correctly.
+     */
+    @Test
+    void checkWithChangeOwnerHelperMethod() throws NoSuchAlgorithmException {
+
+        ArrayList<PublicKey> publicKeys = new ArrayList<>();
+
+        KeyPair farmKeys = TestUtils.generateKey();
+        publicKeys.add(farmKeys.getPublic());
+
+        KeyPair ownerKeys = TestUtils.generateKey();
+        publicKeys.add(ownerKeys.getPublic());
+
+        KeyPair newOwnerKeys = TestUtils.generateKey();
+        publicKeys.add(newOwnerKeys.getPublic());
+
+        BasketOfApples basketOfApples = new BasketOfApples( "Fuji, Aomori.",
+                farmKeys.getPublic(), ownerKeys.getPublic(), 100);
+
+        /* 鍵更新*/
+        BasketOfApples newOwnerBasketOfApples = basketOfApples.changeOwner(newOwnerKeys.getPublic());
+
+        /* Ownerの鍵が更新されていることを確認。 */
+        assertEquals(newOwnerBasketOfApples.getOwner(), newOwnerKeys.getPublic());
+
+        /* 他のフィールドが更新されていないことを確認。 */
+        assertEquals(newOwnerBasketOfApples.getDescription(), basketOfApples.getDescription());
+        assertEquals(newOwnerBasketOfApples.getFarm(), basketOfApples.getFarm());
+        assertEquals(newOwnerBasketOfApples.getWeight(), basketOfApples.getWeight());
+
+    }
 }
