70bb6b5028c33ca7856e5f2ec1dc3e760f6a61cd
kztTateyamaSbi <kzt.tateyama@gmail.com>
2023-08-29 18:28:12 +0900

    add query flow

--- workflows/src/main/java/com/r3/developers/apples/workflows/GetStampIdOfAppleStampByHolderFlow.java
+++ workflows/src/main/java/com/r3/developers/apples/workflows/GetStampIdOfAppleStampByHolderFlow.java
@@ -7,18 +7,20 @@ import net.corda.v5.application.flows.CordaInject;
 import net.corda.v5.application.marshalling.JsonMarshallingService;
 import net.corda.v5.application.membership.MemberLookup;
 import net.corda.v5.application.persistence.PagedQuery;
+import net.corda.v5.base.annotations.Suspendable;
 import net.corda.v5.ledger.utxo.StateAndRef;
 import net.corda.v5.ledger.utxo.UtxoLedgerService;
 import org.jetbrains.annotations.NotNull;
 
 import java.security.PublicKey;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Stream;
 
-public class GetAppleStampByHolderFlow implements ClientStartableFlow {
+public class GetStampIdOfAppleStampByHolderFlow implements ClientStartableFlow {
 
+    String APPLE_STAMP_FULL_PATH = "com.r3.developers.apples.states.AppleStamp";
     @CordaInject
     private JsonMarshallingService jsonMarshallingService;
 
@@ -29,6 +31,7 @@ public class GetAppleStampByHolderFlow implements ClientStartableFlow {
     private UtxoLedgerService utxoLedgerService;
 
     @NotNull
+    @Suspendable
     @Override
     public String call(@NotNull ClientRequestBody requestBody) {
 
@@ -41,33 +44,21 @@ public class GetAppleStampByHolderFlow implements ClientStartableFlow {
             throw new IllegalArgumentException("The holder does not exist within the network");
         }
 
-        int currentOffset = 0;
 
-        List<StateAndRef> resultSet = utxoLedgerService.query("TOKEN_HOLDER_NAMED_QUERY", StateAndRef.class) // instantiate the query
+        PagedQuery.ResultSet<?> resultSet = utxoLedgerService.query("TOKEN_HOLDER_NAMED_QUERY", StateAndRef.class) // instantiate the query
                 .setOffset(0) // Start from the beginning
                 .setLimit(1000) // Only return 1000 records
-                .setParameter("holder", holder.toString()) // Set the parameter to a dummy value
+                .setParameter("holder", holder.toString())
                 .setCreatedTimestampLimit(Instant.now()) // Set the timestamp limit to the current time
-                .execute()
-                .getResults();
-//                .stream()
-//                .filter(result -> result instanceof StateAndRef && ((StateAndRef<?>) result).getState().getContractState() instanceof AppleStamp);
+                .execute();
 
-        Stream AppleStamps = resultSet.stream().filter(result -> result != null && ((StateAndRef<?>) result).getState().getContractState() instanceof AppleStamp);
-        AppleStamps.
+        List<StateAndRef<AppleStamp>> results = (List<StateAndRef<AppleStamp>>) resultSet.getResults();
+        List<String> appleStampId = new ArrayList<>();
 
+        results.forEach( stateAndRefOfStamp ->
+                appleStampId.add(stateAndRefOfStamp.getState().getContractState().getId().toString())
+                );
 
-//        while (resultSet.results.isNotEmpty()) {
-//            currentOffset += 1000;
-//            query.setOffset(currentOffset);
-//            resultSet = query.execute();
-//        }
-
-        List<StateAndRef> results = resultSet.getResults();
-        results.
-
-
-
-        return null;
+        return jsonMarshallingService.format(appleStampId);
     }
 }
