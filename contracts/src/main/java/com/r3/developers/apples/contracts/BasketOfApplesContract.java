e273da3ba49f3cc353ad9d5059c2ab92b96a3959
kztTateyamaSbi <kzt.tateyama@gmail.com>
2023-08-29 20:30:00 +0900

    change contracts default code

--- contracts/src/main/java/com/r3/developers/apples/contracts/BasketOfApplesContract.java
+++ contracts/src/main/java/com/r3/developers/apples/contracts/BasketOfApplesContract.java
@@ -17,61 +17,39 @@ public class BasketOfApplesContract implements Contract {
         }
     }
 
-    @Override
-    public void verify(UtxoLedgerTransaction transaction) {
-
-        Command basketOfApplesCommand = transaction.getCommands(BasketOfApplesContract.BasketOfApplesCommands.class).get(0);
-
-        if(basketOfApplesCommand.getClass() == BasketOfApplesCommands.PackBasket.class) {
-
-//            /* Check the size of input and output state */
-//            requireThat(transaction.getInputStates(BasketOfApples.class).size() == 0,
-//                    "This transaction should not include an BasketOfApples input state.");
-//            requireThat(transaction.getOutputStates(BasketOfApples.class).size() == 1,
-//                    "This transaction should include one BasketOfApples output state.");
-//
-//            /* Check the content of output state*/
-//            BasketOfApples output = transaction.getOutputStates(BasketOfApples.class).get(0);
-//
-//            requireThat(!(output.getDescription().isBlank()),
-//                    "The output BasketOfApples state should have clear description of Apple product");
-//
-//            requireThat(output.getWeight() > 0 ,
-//                "The output BasketOfApples state should have non zero weight"
-//            );
-        }
-        else if(basketOfApplesCommand.getClass() == BasketOfApplesCommands.Move.class) {
-            /* Check the size of input and output state of BasketOfApples */
-//            requireThat(transaction.getInputStates(BasketOfApples.class).size() == 1,
-//                    "This transaction should include one BasketOfApples input state.");
-//            requireThat(transaction.getOutputStates(BasketOfApples.class).size() == 1,
-//                    "This transaction should include one BasketOfApples output state.");
-//
-//            /* Check the size of input state of AppleStamp.
-//            This smart contract can combine AppleStamp and BasketOfApples reimbursements.*/
-//            requireThat(transaction.getInputStates(AppleStamp.class).size() == 1,
-//                    "This transaction should include one AppleStamp input state.");
-//
-//            /* Check the content of output state*/
-//            AppleStamp inputAppleStamp = transaction.getInputStates(AppleStamp.class).get(0);
-//            BasketOfApples inputBasketOfApples = transaction.getInputStates(BasketOfApples.class).get(0);
-//            BasketOfApples outputBasketOfApples = transaction.getOutputStates(BasketOfApples.class).get(0);
-//
-//            requireThat(inputAppleStamp.getIssuer().equals(inputBasketOfApples.getFarm()),
-//                    "The issuer of the Apple stamp should be the producing farm of this basket of apple");
-//
-//            /* Check that the onwer has changed before and after the transaction. */
-//            requireThat(!(inputBasketOfApples.getOwner().equals(outputBasketOfApples.getOwner())),
-//                    "The owner of the input and output should be changed.");
-        }
-        else {
-            throw new CordaRuntimeException("Incorrect type of BasketOfApples commands: " + transaction.getCommands().get(0).getClass());
-        }
+    private void requireThat(boolean asserted, String errorMessage) {
+        //実装①-1
     }
 
-    private void requireThat(boolean asserted, String errorMessage) {
-        if(!asserted) {
-            throw new CordaRuntimeException("Failed requirement: " + errorMessage);
-        }
+    @Override
+    public void verify(UtxoLedgerTransaction transaction) {
+        //実装①-2
+        BasketOfApplesCommands basketOfApplesCommand;
+        //実装①-3
+
+        //実装①-4 PackBasket-個数制約
+        //"This transaction should not include an BasketOfApples input state."
+        //"This transaction should include one BasketOfApples output state."
+
+        //実装①-5 PackBasket-内容制約
+        //"The output BasketOfApples state should have clear description of Apple product"
+        //"The output BasketOfApples state should have non zero weight"
+        //
+
+        //実装①-6 Move-個数制約
+        //"This transaction should include one BasketOfApples input state."
+        //"This transaction should include one BasketOfApples output state."
+
+        //実装①-7 Move-個数制約その２
+        //"This transaction should include one AppleStamp input state."
+
+        //実装①-8 Move-内容制約
+        //AppleStamp inputAppleStamp = transaction.getInputStates(AppleStamp.class).get(0);
+        //BasketOfApples inputBasketOfApples = transaction.getInputStates(BasketOfApples.class).get(0);
+        //BasketOfApples outputBasketOfApples = transaction.getOutputStates(BasketOfApples.class).get(0);
+
+        //"The issuer of the Apple stamp should be the producing farm of this basket of apple"
+        //"The owner of the input and output should be changed."
+        throw new CordaRuntimeException("Incorrect type of BasketOfApples commands: " + transaction.getCommands().get(0).getClass());
     }
 }
